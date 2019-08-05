3f930f1c312900eb71a6bc1e122883b3a5147e08
Michael Chu <mathegan@gmail.com>
2019-08-05 14:19:50 -0700

    complete test message

--- Tests/test_doubly_linked_list.py
+++ Tests/test_doubly_linked_list.py
@@ -18,29 +18,29 @@ class TestDoublyLinkedList(unittest.TestCase):
         list.append("A")
         list.append("B")
         list.append("C")
-        self.assertEqual("C", list.tail.data)
+        self.assertEqual("C", list.tail.data, 'tail should give the last Node in the DLL')
     
     def test_push_empty(self):
         list = DoublyLinkedList()
         list.push("T")
-        self.assertEqual("T", list.head.data)
+        self.assertEqual("T", list.head.data, 'error inserting Node in the front of an empty DLL')
 
     def test_push_non_empty(self):
         list = DoublyLinkedList()
         list.push("A")
         list.push("T")
-        self.assertEqual("T", list.head.data)
+        self.assertEqual("T", list.head.data, 'error inserting Node in the front of a non-empty DLL')
 
     def test_append_empty(self):
         list = DoublyLinkedList()
         list.append("T")
-        self.assertEqual("T", list.head.data)
+        self.assertEqual("T", list.head.data, 'error appending to an empty DLL')
 
     def test_append_non_empty(self):
         list = DoublyLinkedList()
         list.push("A")
         list.append("T")
-        self.assertEqual("T", list.tail.data)
+        self.assertEqual("T", list.tail.data, 'error appending to a non-empty DLL')
 
     def test_insert_after(self):
         list = DoublyLinkedList()
@@ -48,4 +48,4 @@ class TestDoublyLinkedList(unittest.TestCase):
         list.append("B")
         list.append("C")
         list.insert_after(list.head, "T")
-        self.assertEqual("T", list.head.next.data)
+        self.assertEqual("T", list.head.next.data, 'error inserting after a Node in DLL')
